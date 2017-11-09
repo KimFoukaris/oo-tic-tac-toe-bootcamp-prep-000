a674eede074596fc7ccae804509353de201fd087
Kim Foukaris <kimfoukaris@outlook.com>
2017-11-09 16:28:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,4 +37,8 @@ class TicTacToe
   def move(index, current_player)
     @board[index] = current_player
   end
+
+  def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
+end
 end
